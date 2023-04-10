104c28e5942482dd18940db746e588d394fad286
PrabhjotSodhi <sodhiprabhjot23@gmail.com>
2023-04-11 11:35:16 +1200

    Deployed Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/react' ;
 import "./index.css";
 
 const router = createBrowserRouter([
@@ -22,5 +23,6 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
   </React.StrictMode>
 );
